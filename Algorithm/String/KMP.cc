21e4ef4270cf2daf094fb4612d80a6cb8a10b817
hansionz <1776844548@qq.com>
2019-05-12 23:40:14 +0800

    实现KMP匹配算法

--- Algorithm/String/KMP.cc
+++ Algorithm/String/KMP.cc
@@ -1,12 +1,85 @@
 #include <iostream>
+#include <string>
+#include <vector>
 
 using namespace std;
 
-// 子串:必须连续  子序列:可以连续、可以不连续
-// Q:字符串匹配算法(返回str2在str1中的位置)
-// 
+// 子串(子数组):必须连续  子序列:可以连续、可以不连续
+// KMP:是包含问题,返回str2在str1中的位置 eg: str1 = "abcd123" str2 = "cd12" ,返回2
+// Java中getIndexOf(String s1, String s2)是一个KMP算法
+
+// 方法一:暴力法
+// 从str1的第一个位置开始匹配,如果匹配则返回位置，匹配则str1继续向后移动匹配
+// 时间复杂度:O(N*M)(N >= M)
+// 方法二:KMP
+// 思想:利用前边匹配过的信息找后边，为其加速匹配
+// 前提:最长前缀和最长后缀的匹配长度
+//      eg:str = "abcabcd" 前缀不能包含最后一个字符，后缀不能包含最前一个字符
+//         d 位置的信息是 abcabc 的最长前缀和最长后缀的匹配长度为3(abc abc)
+// 思路: 1.求的是str2的每个位置的最长前缀和最长后缀的匹配长度
+//         eg: ababac [0 1 2 3 4 5]-->[-1 0 0 1 2 3]
+//       2.如果str1和str2前边一部分字符串匹配,后边一个位置没有匹配上
+//         则将str2推向str1中最长后缀的位置继续匹配，而最长前缀和最
+//         长后缀匹配，则只需要跳过它们的长度继续匹配即可
+// 实质: i(最长前缀的开始) - j(最长后缀的开始) 这段位置中是无法匹配出来的
+// 为什么: 如果从k(i - j中的任意位置) - x(任意位置) 可以匹配
+//         那必须要它们的前缀等量相同，但这不可能只有到j开始
+//         匹配采用等量的前缀
+
+class Solution
+{
+    public:
+        static int KMP(string str, string sub){
+            if(str.size() < 1 || str.size() < sub.size()){
+                return -1;
+            }
+            int sr = 0;
+            int sb = 0;
+            vector<int> next = getNextArray(sub);
+            while(sr < str.size() && sb < sub.size()){
+                if(str[sr] == sub[sb]){
+                    sr++;
+                    sb++;
+                }else if(next[sb] == -1) {
+                    sr++;
+                }else{
+                    sb = next[sb];
+                }
+            }
+            // 子串匹配完了
+            return sb == sub.size() ? sr - sb : -1;
+        }
+    private:
+        static vector<int> getNextArray(string sub){
+            // 动态规划
+            // i位置的next值是i-1位置上的值
+            // 如果
+            vector<int> next;
+            if(sub.size() == 1){
+                return next;
+            }
+
+            next[0] = -1;
+            next[1] = 0;
+            int i = 2;
+            int cn = 0;
+            while(i < next.size()){
+                if(sub[i - 1] == sub[cn]){
+                    next[i++] = ++cn;
+                }else if(cn > 0){
+                    cn = next[cn];
+                }else{
+                    next[i++] = 0;
+                }
+            }
+            return next;
+        }
+};
+
 int main()
 {
-
+    string str1("abcdesf");
+    string sub("cdes");
+    cout << Solution::KMP(str1, sub) << endl;
     return 0;
 }
--- Nginx/README.md
+++ Nginx/README.md
@@ -65,4 +65,105 @@ gpgkey=https://nginx.org/keys/nginx_signing.key
     - cat nginx.conf(配置文件)
     - cat /etc/nginx/conf.d/*.conf(子配置文件)
 
+#### 4.HTTP 
 
+```c
+客户端--->(request)服务器
+服务器--->(response)客户端
+```
+* Request:包括请求行、请求头部、空行、请求数据
+* Response:包括状态行、响应头部、空行、响应正文
+* 使用`curl -v 域名`可以看到以上信息
+
+**关于HTTP:https://blog.csdn.net/hansionz/article/details/86137260**
+
+#### 5.Nginx日志类型
+
+* error.log:Nginx服务处理HTTP请求错误的状态以及Nginx本身运行出错的状态
+* access.log:记录Nginx每次HTTP请求访问的状态
+* Nginx基于log_format实现这些日志
+
+#### 6.Nginx变量
+
+* HTTP请求变量: arg_PARAMETR、http_HEADER(请求)、sent_http_HEADER(响应)
+* 内置变量:nginx.org官网中有所有变量中的详细说明 remote_addr代表客户端的地址
+* 自定义变量:程序员可以自己定义变量
+
+#### 7.Nginx模块学习
+
+1.Nginx官方模块
+
+* nginx -V 可以查看配置参数
+-----------| ----------------
+编译选项     |     作用
+---------| -------------
+--with-http_stub_status_module | Nginx的客户端状态
+-------- | ------------------------
+--with-http_random_index_module | 目录中选择一个随机主页
+---------- | ----------
+--with-http_sub_module | HTTP内容替换
+------- | --------------
+http_access_module | 用于IP的访问控制模块
+
+2.第三方模块
+
+## Nginx常见的中间间架构
+
+### 三.静态资源WEB服务
+```c
+客户---->(REQ:html、jpeg)-- Nginx ----->静态资源存储
+```
+* 静态资源:服务器直接保存的资源，可以直接返回给客户(HTML、CSS、GIF、TXT等)
+* 动态资源:服务器需要客户端的请求参数进行动态计算得到结果返回给客户端
+
+Nginx作为静态WEB服务器的CDN(内容分发网络)场景:传输时延的最小化
+
+场景:假设北京的用户需要请求一份新疆的静态资源,但是由于优化时延，特别
+     在北京、湖南、山东等多个地方存有该资源的多份备份，当北京的用户去
+     请求时DNS系统根据条件(距离、时间等)从最近的服务器获取资源。
+Ngnix静态资源WEB服务配置语法:
+```c
+Syntax: sendfile on | off
+Default: sendfile off
+Context:http,server,location,if inlocation
+// sendfile:不存在内核到用户的两次拷贝，直接在内核中进行，读取速度快
+// with-file-aio:异步文件读取
+
+// 配置项: tcp_nopush 提高网络传输效率(sendfile开启情况),把多个包一起进行发送提高效率
+Syntax:tcp_nopush on | push
+Default: tcp_nopush off
+Context:http,server,location
+
+// 配置项: tcp_nodelay keep_alive连接下，提高网络传输的实时性
+Syntax:tcp_nodelay on | push
+Default: tcp_nodelay on
+Context:http,server,location
+
+// 配置项:压缩传输,减少传输时间,提高带宽利用率，提高传输效率
+Syntax:gzip on | push
+Default: gzip off
+Context:http,server,location, if in location
+浏览器(解压)《---------》Nginx(压缩)
+
+// 配置项:压缩比率
+Syntax:gzip_comp_level level
+Default: gzip_comp_level 1
+Context:http,server,location
+
+// 扩展Nginx压缩模块
+http_gzip_static_module:预读gzip功能
+http_gunzip_module:应用支持gunzip的压缩方式
+
+//配置项:添加Cache-Control、Expires
+
+Syntax:expires [modified] time
+       expires epoch | max | off
+Default: expires off
+Context:http,server,location, if in location
+```
+
+### 四.代理服务
+
+### 五.负载均衡调度器SLB
+
+### 六.动态缓存
